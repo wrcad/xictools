c4351ac99685b7a94649c29e6bcfdb841931d1af
Stephen R. Whiteley <stevew@wrcad.com>
2017-08-03 16:29:41 -0700

    More header updates.

--- wrspice/devlib/bsimsoi-4.0/b4sic.cc
+++ wrspice/devlib/bsimsoi-4.0/b4sic.cc
@@ -1,25 +1,34 @@
 
 /*========================================================================*
  *                                                                        *
- *  XICTOOLS Integrated Circuit Design System                             *
- *  Copyright (c) 1996 Whiteley Research Inc, all rights reserved.        *
+ *  Whiteley Research Inc, Sunnyvale, CA USA, http://wrcad.com            *
+ *  Copyright (C) 2017 Whiteley Research Inc, all rights reserved.        *
  *                                                                        *
+ *  Licensed under the Apache License, Version 2.0 (the "License");       *
+ *  you may not use this file except in compliance with the License.      *
+ *  You may obtain a copy of the License at                               *
+ *                                                                        *
+ *        http://www.apache.org/licenses/LICENSE-2.0                      *
+ *                                                                        *
+ *  See the License for the specific language governing permissions       *
+ *  and limitations under the License.                                    *
  *                                                                        *
  *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      *
  *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES      *
  *   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-        *
- *   INFRINGEMENT.  IN NO EVENT SHALL STEPHEN R. WHITELEY BE LIABLE       *
- *   FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION      *
- *   OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN           *
- *   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN         *
- *   THE SOFTWARE.                                                        *
+ *   INFRINGEMENT.  IN NO EVENT SHALL WHITELEY RESEARCH INCORPORATED      *
+ *   OR STEPHEN R. WHITELEY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER     *
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,      *
+ *   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE       *
+ *   USE OR OTHER DEALINGS IN THE SOFTWARE.                               *
  *                                                                        *
  *========================================================================*
+ *               XicTools Integrated Circuit Design System                *
  *                                                                        *
- * Device Library                                                         *
+ * WRspice Circuit Simulation and Analysis Tool:  Device Library          *
  *                                                                        *
  *========================================================================*
- $Id: b4sic.cc,v 1.2 2011/12/18 01:15:39 stevew Exp $
+ $Id:$
  *========================================================================*/
 
 /***  B4SOI 11/30/2005 Xuemei (Jane) Xi Release   ***/
--- wrspice/devlib/util/parmcvt.cc
+++ wrspice/devlib/util/parmcvt.cc
@@ -1,3 +1,36 @@
+
+/*========================================================================*
+ *                                                                        *
+ *  Whiteley Research Inc, Sunnyvale, CA USA, http://wrcad.com            *
+ *  Copyright (C) 2017 Whiteley Research Inc, all rights reserved.        *
+ *                                                                        *
+ *  Licensed under the Apache License, Version 2.0 (the "License");       *
+ *  you may not use this file except in compliance with the License.      *
+ *  You may obtain a copy of the License at                               *
+ *                                                                        *
+ *        http://www.apache.org/licenses/LICENSE-2.0                      *
+ *                                                                        *
+ *  See the License for the specific language governing permissions       *
+ *  and limitations under the License.                                    *
+ *                                                                        *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      *
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES      *
+ *   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-        *
+ *   INFRINGEMENT.  IN NO EVENT SHALL WHITELEY RESEARCH INCORPORATED      *
+ *   OR STEPHEN R. WHITELEY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER     *
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,      *
+ *   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE       *
+ *   USE OR OTHER DEALINGS IN THE SOFTWARE.                               *
+ *                                                                        *
+ *========================================================================*
+ *               XicTools Integrated Circuit Design System                *
+ *                                                                        *
+ * WRspice Circuit Simulation and Analysis Tool:  Device Library          *
+ *                                                                        *
+ *========================================================================*
+ $Id:$
+ *========================================================================*/
+
 // usage: parmcvt < infile
 //
 // This takes a Berlekey-style IFparm definition for device/model
