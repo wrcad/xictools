5250449836e94d82977ad71b0992efb70660bfba
Stephen R. Whiteley <stevew@wrcad.com>
2018-08-14 10:37:18 -0700

    Xic:  doc tweaks, bug fix.  Highlighted bus terminals could not be un-highlighted.

--- xic/src/display/dsp_mark.cc
+++ xic/src/display/dsp_mark.cc
@@ -1360,7 +1360,7 @@ cDisplay::HliteElecBsc(bool display, const CDp_bsnode *pn)
             sMark *mp = 0, *mn;
             for (sMark *mm = MK.hlite_list; mm; mm = mn) {
                 mn = mm->mNext;
-                if (mm->mType == (symb ? MARK_STERM : MARK_ETERM) &&
+                if (mm->mType == (symb ? MARK_SYBSC : MARK_BSC) &&
                         mm->mX == x && mm->mY == y &&
                         mm->mColor == SelectColor) {
                     if (!mp)
