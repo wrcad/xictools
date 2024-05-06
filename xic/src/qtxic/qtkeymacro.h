99d0499ff902e4e4cc49d867b0ca2c797e0b7bc4
Stephen Whiteley <stevew@wrcad.com>
2024-05-06 07:54:59 -0700

    Xic,QT:  new file, forgot in last commit.

---
+++ xic/src/qtxic/qtkeymacro.h
@@ -0,0 +1,65 @@
+
+/*========================================================================*
+ *                                                                        *
+ *  Distributed by Whiteley Research Inc., Sunnyvale, California, USA     *
+ *                       http://wrcad.com                                 *
+ *  Copyright (C) 2017 Whiteley Research Inc., all rights reserved.       *
+ *  Author: Stephen R. Whiteley, except as indicated.                     *
+ *                                                                        *
+ *  As fully as possible recognizing licensing terms and conditions       *
+ *  imposed by earlier work from which this work was derived, if any,     *
+ *  this work is released under the Apache License, Version 2.0 (the      *
+ *  "License").  You may not use this file except in compliance with      *
+ *  the License, and compliance with inherited licenses which are         *
+ *  specified in a sub-header below this one if applicable.  A copy       *
+ *  of the License is provided with this distribution, or you may         *
+ *  obtain a copy of the License at                                       *
+ *                                                                        *
+ *        http://www.apache.org/licenses/LICENSE-2.0                      *
+ *                                                                        *
+ *  See the License for the specific language governing permissions       *
+ *  and limitations under the License.                                    *
+ *                                                                        *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      *
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES      *
+ *   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-        *
+ *   INFRINGEMENT.  IN NO EVENT SHALL WHITELEY RESEARCH INCORPORATED      *
+ *   OR STEPHEN R. WHITELEY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER     *
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,      *
+ *   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE       *
+ *   USE OR OTHER DEALINGS IN THE SOFTWARE.                               *
+ *                                                                        *
+ *========================================================================*
+ *               XicTools Integrated Circuit Design System                *
+ *                                                                        *
+ * Xic Integrated Circuit Layout and Schematic Editor                     *
+ *                                                                        *
+ *========================================================================*
+ $Id:$
+ *========================================================================*/
+
+#ifndef QTKEYMACRO_H
+#define QTKEYMACRO_H
+
+namespace qt_keyb {
+
+    // List element for widget name matches.
+    //
+    struct wlist
+    {
+        wlist(QWidget *o, wlist *n) : widget(o), next(n) { }
+
+        QWidget *widget;
+        wlist *next;
+    };
+
+    // qtkeymacro.cc
+    bool getkey_event_handler(QObject*, QEvent*, void*);
+    bool macro_event_handler(QObject*, QEvent*, void*);
+    char *widget_path(const QWidget*);
+    wlist *find_widget(const QWidget*, const char*);
+    QWidget *name_to_widget(const char*);
+}
+
+#endif
+
